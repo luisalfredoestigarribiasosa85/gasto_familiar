394d55246db388c5f83bb76eab42526c8f6499c0
luisalfredoestigarribiasosa85 <luisalfredoestigarribiasosa85@gmail.com>
2024-08-23 20:45:03 -0400

    new

---
+++ scripts/app.js
@@ -0,0 +1,43 @@
+document.getElementById('gastoForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    // Obtener los valores del formulario
+    const descripcion = document.getElementById('descripcion').value;
+    const monto = document.getElementById('monto').value;
+
+    // Crear un nuevo elemento en la lista
+    const li = document.createElement('li');
+    li.className = 'list-group-item';
+    li.textContent = `${descripcion}: ${monto} Gs`;
+
+    // Añadir el nuevo gasto a la lista
+    document.getElementById('listaGastos').appendChild(li);
+
+    // Limpiar el formulario
+    document.getElementById('gastoForm').reset();
+
+    // Enviar los datos a Google Sheets
+    enviarAGoogleSheets(descripcion, monto);
+});
+
+function enviarAGoogleSheets(descripcion, monto) {
+    // Aquí añadiremos el código para enviar los datos a Google Sheets
+    const url = "https://script.google.com/macros/s/AKfycbzW4jNJ6y_7dW52BB7AeiH0WovgqDSoqKXjQZDBij6h-fg1IjZiRkOSv00vTi6eP1gpig/exec"; // Reemplaza con tu URL de Google Apps Script
+
+    fetch(url, {
+        method: 'POST',
+        mode: 'no-cors',
+        body: JSON.stringify({ descripcion: descripcion, monto: monto }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Datos enviados a Google Sheets', data);
+        })
+        .catch(error => {
+            console.error('Error al enviar los datos', error);
+        });
+}
+
