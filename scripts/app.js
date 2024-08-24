e57b06e830326c28ac0f1118344d52ea918cc94b
luisalfredoestigarribiasosa85 <luisalfredoestigarribiasosa85@gmail.com>
2024-08-24 14:59:19 -0400

    All added

--- scripts/app.js
+++ scripts/app.js
@@ -16,31 +16,25 @@ document.getElementById('gastoForm').addEventListener('submit', function (event)
     // Limpiar el formulario
     document.getElementById('gastoForm').reset();
 
-    // Enviar los datos a Google Sheets
+    // Enviar los datos a Google Sheets a través del servidor intermedio
     enviarAGoogleSheets(descripcion, monto);
 });
 
 function enviarAGoogleSheets(descripcion, monto) {
-    const url = "https://script.google.com/macros/s/AKfycbx1_EyBH_g6q8S4Kcp3XAahascFSKODV-coYwtB4U2SdPsmNRs1eaCPsszWOcQjdk7Tow/exec"; // Reemplaza con tu URL de Google Apps Script
+    const url = 'http://localhost:3000/enviar'; // Cambia la URL según sea necesario
 
     fetch(url, {
         method: 'POST',
-        mode: 'cors', // Permitir solicitudes CORS
         body: JSON.stringify({ descripcion: descripcion, monto: monto }),
         headers: {
             'Content-Type': 'application/json'
         }
     })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Error en la respuesta del servidor');
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log('Datos enviados a Google Sheets', data);
-        })
-        .catch(error => {
-            console.error('Error al enviar los datos', error);
-        });
+    .then(response => response.json())
+    .then(data => {
+        console.log('Datos enviados a Google Sheets', data);
+    })
+    .catch(error => {
+        console.error('Error al enviar los datos', error);
+    });
 }
